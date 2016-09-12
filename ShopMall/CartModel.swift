29a4e0fda733c30bdeafb25385b45819116d9062
admin <admin@admins-MacBook-Pro.local>
2016-09-11 18:39:15 -0700

    Added shopping cart with name, picture, price, and qty.

---
+++ ShopMall/CartModel.swift
@@ -0,0 +1,22 @@
+//
+//  CartModel.swift
+//  ShopMall
+//
+//  Created by admin on 9/10/16.
+//  Copyright © 2016 letsbuildthatapp. All rights reserved.
+//
+
+import UIKit
+
+class CartModel: NSObject {
+    var cartDictionary: NSNumber?
+    
+    
+    init(dictionary: [String: AnyObject]) {
+        super.init()
+        
+        
+    }
+    
+    
+}
